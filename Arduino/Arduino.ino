3b84c366f49359e7f1fd71753006252925143cd3
unknown <user@USER.Laptop>
2020-04-04 09:48:36 +0200

    More comments and more examples

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -11,7 +11,9 @@ void setup() {
   attachInterrupt(ButtonsA.Data.PIN_Button, ISR_ButtonsA, CHANGE);  //Attack an interupt to the pin, so we can simply keep track of pushes
 }
 void loop() {
-  Button_Time Value = ButtonsA.CheckButton();
+  Button_Time Value = ButtonsA.CheckButton();   //Get/update the button state
+
+  /* Print all state values to serial for debugging */
   Serial.print(Value.StartPress);
   Serial.print("_");
   Serial.print(Value.Pressed);
@@ -28,14 +30,28 @@ void loop() {
   Serial.print("_");
   Serial.print(Value.PressEnded);
   Serial.print(" ");
-
   Serial.print(Value.PressedTime);
   Serial.print(" ");
   Serial.println();
-  delay(100);
+
+  delay(100);             //Just some time to make sure so humans can read the serial a bit better (Yes I know delays are bad)
+
+  /* Just some more examples of how to use it */
+  if (Value.StartPress) {
+    Serial.println("The button is just pressed");
+  }
+  if (Value.StartLongPress) {
+    Serial.println("The button is just pressed for a long time");
+  }
+  if (Value.StartDoublePress) {
+    Serial.println("The button is just double pressed");
+  }
+  if (Value.StartRelease) {
+    Serial.println("The button has just been released");
+  }
 }
 
-//ISR must return nothing and take no arguments, so we need this sh*t
+/* ISR must return nothing and take no arguments, so we need this sh*t */
 void ISR_ButtonsA() {
   ButtonsA.Pinchange();
 }
